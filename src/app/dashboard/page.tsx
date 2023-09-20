f36ea27cb4d96000857fb83087f5c2f0e309f672
ophx <castytastyhappybat@gmail.com>
2023-09-20 18:55:04 -0400

    fix

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
     return (
         <>
             {/* Navbar */}
-                <Navbar />
+            <Navbar />
 
             { /* Main */ }
                 <main className="container mx-auto px-64">
