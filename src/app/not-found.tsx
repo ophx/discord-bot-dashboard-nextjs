9346d1f2fe70d80442134414bac25369133e1dbf
ophx <castytastyhappybat@gmail.com>
2023-09-20 23:39:55 -0400

    loading animation

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -10,7 +10,7 @@ export default function NotFound() {
             <div className="flex justify-between items-center">
               <div className="w-full">
                 <p className="text-white text-5xl">Not Found!</p>
-                <p className="text-gray-400 text-xl">The page you requested was not found!</p>
+                <p className="text-gray-400 text-xl">The Page You Requested Was Not Found!</p>
                 <div className="flex gap-4 mt-2 justify-center">
                   <Link href="/" className="transition-all duration-150 px-4 py-2 rounded shadow-lg bg-blue-600 hover:bg-blue-700 text-white">
                     Go Back Home
